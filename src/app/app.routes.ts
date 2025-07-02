da897b675365e461c86b7d9fdd7864f376e28a43
Daniel Sanchez Leon(UST <297920@ust.com>
2025-07-02 10:42:49 +0200

    Se añaden las rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { CreateAlertComponent } from './features/create-alert/create-alert.component';
+import { CreateSimpleConditionAlertComponent } from './features/create-simple-condition-alert/create-simple-condition-alert.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'crear-alerta',
+    component: CreateAlertComponent,
+  },
+  {
+    path: 'condicion-simple',
+    component: CreateSimpleConditionAlertComponent,
+  }
+];
