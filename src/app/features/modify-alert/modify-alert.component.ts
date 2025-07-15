906eab4fbf98c3ca0e541639b1c0505f6e2a9f5e
Daniel Sanchez Leon(UST <297920@ust.com>
2025-07-15 13:51:18 +0200

    Generado el contenedor de filtros y arreglados problemas varios.

--- src/app/features/modify-alert/modify-alert.component.ts
+++ src/app/features/modify-alert/modify-alert.component.ts
@@ -12,21 +12,38 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { InputNumberModule } from 'primeng/inputnumber';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PopoverModule } from 'primeng/popover';
 import { SelectModule } from 'primeng/select';
 import { thresholdTypeOptions, thresholdComparationOptions } from '../../shared/constants/threshold-options';
 import { timeWindowOptions, periodicityOptions } from '../../shared/constants/metric-options';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-modify-alert',
   imports: [FormsModule, SelectModule, PopoverModule, ReactiveFormsModule, InputNumberModule, MultiSelectModule, FloatLabelModule, InputTextModule, InputIconModule, IconFieldModule, MenuModule, TableModule, CommonModule, ButtonModule, PageWrapperComponent],
   templateUrl: './modify-alert.component.html',
-  styleUrl: './modify-alert.component.scss'
+  styleUrl: './modify-alert.component.scss',
+  animations: [
+    trigger('accordionAnimation', [
+      state('closed', style({
+        height: '0px',
+        padding: '0 1rem',
+        opacity: 0,
+      })),
+      state('open', style({
+        height: '*',
+        padding: '1rem',
+        opacity: 1,
+      })),
+      transition('closed <=> open', [
+        animate('300ms ease-in-out')
+      ])
+    ])
+  ]
 })
-export class ModifyAlertComponent 
-{
+export class ModifyAlertComponent {
   testAlerts: any[] = [];
   filteredTestAlerts: any[] = [];
   filterPanelOpen: boolean = false;
@@ -39,16 +56,23 @@ export class ModifyAlertComponent
 
   tagForm: FormGroup;
 
-  constructor(private router: Router, private _fb: FormBuilder) 
-  {
+  filterForm: FormGroup;
+
+  constructor(private router: Router, private _fb: FormBuilder) {
     this.tagForm = this._fb.group({
       name: ['', [Validators.required]],
       value: ['', [Validators.required]]
     });
+
+    this.filterForm = this._fb.group({
+      name: new FormControl(''),
+      message: new FormControl(''),
+      timeWindow: new FormControl(null),
+      periodicity: new FormControl(null)
+    });
   }
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.testAlerts = testAlerts;
     this.filteredTestAlerts = testAlerts;
 
@@ -59,32 +83,57 @@ export class ModifyAlertComponent
     this.periodicityOptions = periodicityOptions;
   }
 
-  onClickNavigateToHome()
-  {
+  onClickNavigateToHome() {
     this.router.navigate(['']);
   }
 
-  applyFilterGlobal($event: any, stringVal: any) 
-  {
+  applyFilterGlobal($event: any, stringVal: any) {
     const filterValue: string = ($event.target as HTMLInputElement).value.toLowerCase();
 
-    this.filteredTestAlerts = this.testAlerts.filter( (testAlert) =>
-        Object.values(testAlert).some(value =>
-            String(value).toLowerCase().includes(filterValue)
-        )
+    this.filteredTestAlerts = this.testAlerts.filter((testAlert) =>
+      Object.values(testAlert).some(value =>
+        String(value).toLowerCase().includes(filterValue)
+      )
     );
   }
 
-  onClickAddTag(testAlert: any)
-  {
-    testAlert.definedTags.push({name: this.tagForm.get('name')?.value, value: this.tagForm.get('value')?.value});
+  onClickAddTag(testAlert: any) {
+    testAlert.definedTags.push({ name: this.tagForm.get('name')?.value, value: this.tagForm.get('value')?.value });
 
     this.tagForm.reset();
     this.tagForm.updateValueAndValidity();
   }
 
-  onClickRemoveTag(testAlert: any, i: number)
-  {
+  onClickRemoveTag(testAlert: any, i: number) {
     testAlert.definedTags.splice(i, 1);
+  }
+
+  clearIndividualFilters() {
+    this.filterForm.reset();
+    this.filterForm.updateValueAndValidity();
+
+    this.applyIndividualFilters();
+  }
+
+  applyIndividualFilters() 
+  {
+    const filters = this.filterForm.value;
+
+    this.filteredTestAlerts = this.testAlerts.filter((testAlert) => 
+    {
+      const matchesName =
+        !filters.name || testAlert.name.toLowerCase().includes(filters.name.toLowerCase());
+
+      const matchesMessage =
+        !filters.message || testAlert.description.toLowerCase().includes(filters.message.toLowerCase());
+
+      const matchesTimeWindow =
+        !filters.timeWindow || testAlert.timeWindow === filters.timeWindow;
+
+      const matchesPeriodicity =
+        !filters.periodicity || testAlert.periodicity === filters.periodicity;
+
+      return matchesName && matchesMessage && matchesTimeWindow && matchesPeriodicity;
+    });
   }
 }
